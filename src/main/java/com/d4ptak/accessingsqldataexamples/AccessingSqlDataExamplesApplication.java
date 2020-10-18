6e8a6f89318241986b8b0bb2f85c3df5ca98f068
d4ptak <d4ptak@gmail.com>
2020-10-18 16:04:58 +0200

    Init project structure

---
+++ src/main/java/com/d4ptak/accessingsqldataexamples/AccessingSqlDataExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.d4ptak.accessingsqldataexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingSqlDataExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessingSqlDataExamplesApplication.class, args);
+	}
+
+}
